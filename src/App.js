8cb4f972d526aceef0542e328cd80256ac2716bb
Saïd VELO <saidali.velo@gmail.com>
2020-10-15 13:06:35 +0200

    structure without links

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import { Grommet, Box, Text } from 'grommet';
 
-import Home from './features/Components/Home';
-import { Counter } from './features/counter/Counter';
-
-const theme = {
-  global: {
-    colors: {
-      brand: '#228BE6',
-    },
-    font: {
-      family: 'Roboto',
-      size: '18px',
-      height: '20px',
-    },
-  },
-};
+import Responsive from './features/Components/Responsive';
 
 function App() {
   return (
-    <Grommet theme={theme}>
-      <Home></Home>
-    </Grommet>
+      <Responsive></Responsive>
   );
 }
 
